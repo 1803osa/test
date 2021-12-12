3628b73b5329cbaa24d83b39862c5733ddeb3624
1803osa <95528233+1803osa@users.noreply.github.com>
2021-12-12 22:30:14 +0700

    Update login_test.rb

--- login_test.rb
+++ login_test.rb
@@ -3,11 +3,17 @@ require 'selenium-webdriver'
 require 'test/unit'
 include Test::Unit::Assertions
 
+puts "Enter Username: "
+userID = gets
+puts "Enter Password: "
+pass = gets
+
 driver = Selenium::WebDriver.for :edge
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-userID = "ID"
-pass = "PASS"
+# userID = "ID"
+# pass = "PASS"
 cartItem = "ITEM"
+
 
 driver.navigate.to "https://www.tokopedia.com/"
 
@@ -26,14 +32,24 @@ enterPass.send_keys +pass
 login = wait.until {driver.find_element(xpath: "//span[@aria-label= 'login-button']")}
 login.submit
 
+verify = wait.until {driver.find_element(xpath: "//img[@src = 'https://ecs7.tokopedia.net/otp/cotp/ICON_EMAIL_NEW.png']")}
+verify.click
+
+puts "Enter verification code: "
+verificationCode = gets
+
+otp = wait.until {driver.find_element(xpath: "//input[@autocomplete = 'one-time-code']")}
+otp.send_keys +verificationCode	
+
 inputItemName = wait.until {driver.find_element(xpath: "//input[@data-unify= 'Search']")}
 inputItemName.send_keys +cartItem
 
-searchItem = wait.until {driver.find_element(xpath: "//button[@aria-label= 'Tombol Pencarian']")}
+searchItem = wait.until {driver.find_element(xpath: "//button[@aria-label= 'Tombol pencarian']")}
 searchItem.submit
 
-#findCorrectItem = wait.until {driver.find_element(xpath: "//a[contains(@title= 'IPhone 13 Pro Max')])}
+#findCorrectItem = wait.until {driver.find_element(xpath: "//a[contains(@title= 'IPhone 13 Pro Max')]")}
 #findCorrectItem.click
 
 #addToCart = wait.until {driver.find_element(xpath: "//button[@data-testid = 'pdpBtnNormalPrimary']")}
 #addToCart.click
+
