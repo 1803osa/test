3bdf188d4c406f9175d471cf818b0e4e94164e87
alpha.ferry <alpha.ferry@bukalapak.com>
2021-12-13 20:57:07 +0700

    Use chrome instead

--- login_test.rb
+++ login_test.rb
@@ -8,7 +8,7 @@ userID = gets
 puts "Enter Password: "
 pass = gets
 
-driver = Selenium::WebDriver.for :edge
+driver = Selenium::WebDriver.for :chrome
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 # userID = "ID"
 # pass = "PASS"
