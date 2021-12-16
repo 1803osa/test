9db2de8fad47e3ecede1855d109c4cc08653025c
abey <akwilagg@gmail.com>
2021-12-16 21:23:37 +0700

    changed to some variables and methods -changed some variables to constant -added main method to run the test

--- login_test.rb
+++ login_test.rb
@@ -3,40 +3,33 @@ require 'selenium-webdriver'
 require 'test/unit'
 include Test::Unit::Assertions
 
-url = "https://accounts.bukalapak.com/login?"
-
-def get_data
-	puts "Enter email: "
-	user = gets
-	puts "Enter password: "
-	pass = gets
-	puts "Enter item to add to cart"
-	item = gets
-end
+driver = Selenium::WebDriver.for :chrome
+Login_url = "https://accounts.bukalapak.com/login?"
+User = "user"
+Pass = "pass"
+Item_to_add = "item"
+Login_button = "//a[@class= 'sigil-header__nav te-header-login']"
+Email_field = "//div[@dclass= 'bl-text-field__boxed']"
+To_password_field = "//button[@id= 'submit_button']"
+Password_field = "//div[@class= 'bl-text-field__boxed']"
+Login = "//div[@id= 'btn-login']"
+Search_field = "//input[@name= 'search[keywords]']"
+Search_button = "//button[@title= 'Cari']"
+Correct_item = "//a[contains(@title= '#{Item_to_add}')]"
+Cart_button = "//button[contains(@class = 'c-main-product__action__cart')]"
 
 #navigate function
 def open_website (web_name)
-	driver = Selenium::WebDriver.for :chrome
 	driver.navigate.to web_name
 end
 
 def wait (wait_time)
-	Selenium::WebDriver::Wait.new(:timeout => wait_time.to_i)
+	Selenium::WebDriver::Wait.new(:timeout => wait_time)
 end
 
 def pathfinder(path, wait_time = 10)
-	driver = Selenium::WebDriver.for :chrome
-	wait(wait_time).until{driver.find_element(xpath: path.to_s)}
+	wait(wait_time).until{driver.find_element(xpath: path)}
 end
-
-#def nav (type, path, text = "")
-#	if type == "click"
-#		pathfinder(path).click
-#	elsif type == "fill"
-#		pathfinder(path).send_keys +text
-#	elsif type == "submit"
-#		pathfinder(path).submit			
-#end
 
 def click_item(path)
 	pathfinder(path).click
@@ -50,39 +43,31 @@ def submit_data(path)
 	pathfinder(path).submit
 end
 
-
-def automatic_login_test (web_name, email, password)
-	login_button = "//a[@class= 'sigil-header__nav te-header-login']"
-	#login_button = path("button","id","submit_button")
-	email_field = "//div[@dclass= 'bl-text-field__boxed']"
-	#email_field = path("div","class","bl-text-field__boxed")
-	to_password_field = "//button[@id= 'submit_button']"
-	#to_password_field = path("button","id","submit_button")
-	password_field = "//div[@class= 'bl-text-field__boxed']"
-	#password_field = path("div","class","bl-text-field__boxed")
-	login = "//div[@id= 'btn-login']"
-	#login = path("div","id","btn-login")
-
-	open_website(web_name)
+def automatic_login_test
+	open_website(Login_url)
 	#click_item(login_button)
-	input_text(email, email_field)
-	click_item(to_password_field)
-	input_text(password, password_field)
-	submit_data(login)	
+	input_text(User, Email_field)
+	click_item(To_password_field)
+	input_text(Pass, Password_field)
+	submit_data(Login)	
 end
 
-def automatic_add_item_to_cart_test (item_to_add)
-	search_field = "//input[@name= 'search[keywords]']"
-	search_button = "//button[@title= 'Cari']"
-	correct_item = "//a[contains(@title= '#{item_to_add}')]"
-	cart_button = "//button[contains(@class = 'c-main-product__action__cart')]"
-	
-	input_text(item_to_add, search_field)
-	click_item(search_button)
+def automatic_add_item_to_cart_test	
+	input_text(item_to_add, Search_field)
+	click_item(Search_button)
 	#click_item(correct_item)
 	#click_item(cart_button)
 end
 
-#get_data
-automatic_login_test(url, "user", "pass")
-automatic_add_item_to_cart_test("item")
+def run_test
+	automatic_login_test
+	if automatic_login_test !=true
+		puts "Login failed"
+	end
+	automatic_add_item_to_cart_test
+	if automatic_add_item_to_cart_test != true
+		puts "Add item to cart failed"
+	end
+end
+
+run_test
